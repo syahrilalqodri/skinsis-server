91e79fab7e163a0281fb1c1ca99fce7b0640eaab
Muhammad Syahril Alqodri <66176313+syahrilalqodri@users.noreply.github.com>
2022-06-13 02:04:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def predict():
             return res
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
+    app.run(port=int(os.environ.get("PORT", 5000)),host='0.0.0.0',debug=True)
 
 
 
